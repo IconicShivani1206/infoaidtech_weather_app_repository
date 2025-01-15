dd48ab1c1d31c70ff937dd3286bdda429589ff45
IconicShivani1206 <shivigupta12062004@gmail.com>
2025-01-15 20:50:25 +0530

    Kardiya

--- aidtech_weather_appP_repository/weather_main.py
+++ aidtech_weather_appP_repository/weather_main.py
@@ -16,7 +16,7 @@ my_label.pack()
 player = tkvideo("bg vdo.mp4", my_label, loop = 1, size = (1000,300))
 player.play()
 
-print("I have done some change in master so that it should trigger")
+print("Bhai Kitni baar change karu??")
 
 def getWeather():
     ct=entry.get()    
